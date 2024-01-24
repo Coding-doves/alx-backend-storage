6d6b76a563b1e45d315fb447ad4e59608fdaadc0
dove <obenedicta4@gmail.com>
2024-01-24 19:00:00 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,8 +12,36 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        ''' generate a random key (e.g. using uuid) '''
+
+    def store(self,
+              data: Union[str, bytes, int, float]
+              ) -> str:
+        ''' 
+        generate a random key 
+        (e.g. using uuid)
+        '''
         stored_key = str(uuid.uuid4())
         self._redis.set(stored_key, data)
         return stored_key
+
+
+    def get(self,
+            key: str,
+            fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        '''comment'''
+        self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str) -> str:
+        '''
+        correct conversion function
+        '''
+
+    def get_int(self,):
+        '''
+        correct conversion function
+        '''
